53cc04aec22eba0f68462f357afe61647f33687c
kvalium <julien.monchany@gmail.com>
2022-06-23 21:49:20 +0200

    add icon

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/Restaurants.screen";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./src/infrastructure/theme";
-import { useFonts } from "expo-font";
-import { Oswald_400Regular } from "@expo-google-fonts/oswald";
+/* eslint-disable react/no-unstable-nested-components */
 import { Lato_400Regular } from "@expo-google-fonts/lato";
-import { Text } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
+import { Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  NavigationContainer,
+  ParamListBase,
+  RouteProp,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import React from "react";
+import { Text } from "react-native-paper";
+import { ThemeProvider } from "styled-components";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/Restaurants.screen";
 import { SettingsScreen } from "./src/features/settings/screens/Settings.screen";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,14 +27,33 @@ export default function App() {
     return <Text>Loading...</Text>;
   }
 
+  const TAB_ICONS: Record<string, keyof typeof Ionicons.glyphMap> = {
+    Restaurants: "restaurant",
+    Map: "map",
+    Settings: "settings",
+  };
+
   const Tab = createBottomTabNavigator();
+
+  const screenOptions = ({
+    route,
+  }: {
+    route: RouteProp<ParamListBase, string>;
+  }) => ({
+    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+      <Ionicons name={TAB_ICONS[route.name]} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  });
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator screenOptions={screenOptions}>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={SettingsScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
           </Tab.Navigator>
         </NavigationContainer>
